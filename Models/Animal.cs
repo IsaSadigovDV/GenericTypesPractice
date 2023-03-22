634382f8f0ab07191e262cb3dc14d71219e5ae7f
IsaSadigovDV <isans@code.edu.az>
2023-03-22 21:52:06 +0400

    done

---
+++ Models/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTypes.Models
+{
+    internal class Animal<T> where T : class, new()
+    {
+        List<T> list = new List<T>();
+
+        public void Create(T t)
+        {
+            list.Add(t);
+        }
+
+        public void Remove(T t)
+        {
+            list.Remove(t);
+        }
+
+        public void Show(T t)
+        {
+            foreach (T item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        internal void Create(object t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using GenericTypes.Models;
+// queue FiFo - first in first out
+/*Queue<int> queue = new Queue<int>();
+
+queue.Enqueue(1);
+queue.Enqueue(2);
+queue.Enqueue(3);
+queue.Enqueue(4);
+queue.Enqueue(5);
+
+
+
+foreach (int item in queue)
+{
+    Console.WriteLine(item);
+}
+*/
+
+//*********************************************
+
+//stack LiFo - last in first out
+
+/*Stack<int> ints= new Stack<int>();
+
+ints.Push(33);
+ints.Push(12);
+ints.Push(87);
+ints.Push(61);
+
+ints.Pop();
+foreach (var item in ints)
+{
+    Console.WriteLine(item);
+}
+*/
+
+//*********************************************
+
+//sortedlist - siralaryir . meselen a-z, reqemleri azdan coxa
+
+//Dictionarylist - ise oldugu kimi saxlayir
+
+
+SortedList<string, string> sorted = new SortedList<string, string>();
+
+sorted.Add("Ru", "rub");
+sorted.Add("tr", "tl");
+sorted.Add("aze", "azn");
+
+foreach (var item in sorted)
+{
+    Console.WriteLine(item.Key + ":" + item.Value);
+}
